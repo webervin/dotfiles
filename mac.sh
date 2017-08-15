cc2cae337c89fc940041d8444716246ff1ee9072
ervin <ervin@toggl.com>
2017-08-15 21:31:02 +0300

    added profile file

--- mac.sh
+++ mac.sh
@@ -15,3 +15,12 @@ save_heredoc_in(){
 
 (xcode-select --install &&  read -p "Press [Enter] key when xcode finished install") || /usr/bin/true
 
+save_heredoc_in "$HOME/.profile" <<'HEREDOC'
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+HEREDOC
+
+source "$HOME/.profile"
+
+
+echo "All done"
