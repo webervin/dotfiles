a06c501f45e99d7fc85b08ae971b4d1ab1ef024d
ervin <ervin@toggl.com>
2017-08-15 21:38:04 +0300

    add brew

--- mac.sh
+++ mac.sh
@@ -18,9 +18,19 @@ save_heredoc_in(){
 save_heredoc_in "$HOME/.profile" <<'HEREDOC'
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+
+# Brew:
+export PATH="$HOME/tools/brew/bin:$PATH"
+
 HEREDOC
 
 source "$HOME/.profile"
 
+if [ ! -d "$HOME/tools/brew" ]; then
+  mkdir -vp "$HOME/tools"
+  git clone https://github.com/Homebrew/homebrew "$HOME/tools/brew"
+fi
+
+brew update
 
 echo "All done"
