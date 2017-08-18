06ec234047853bf2b6bc3f4fccbc77d9759f4cc4
Ervin <ervin@toggl.com>
2017-08-18 16:29:28 +0300

    enable bash-git-prompt

--- mac.sh
+++ mac.sh
@@ -35,6 +35,13 @@ export PATH="vendor/binstubs:$PATH"
 if [ -f "$HOME/.work_profile" ]; then
   source "$HOME/.work_profile"
 fi
+
+# git prompt
+if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+  __GIT_PROMPT_DIR="$(brew --prefix)/opt/bash-git-prompt/share"
+  source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+fi
+
 HEREDOC
 
 source "$HOME/.profile"
